23191dcc354023ec2304ba13cf7a10c00f206dfd
Aşkın Kesik <140115554+askinksk@users.noreply.github.com>
2023-09-29 14:47:46 +0300

    Enum usage

---
+++ Enums/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    enum Gun { Pazartesi=1,Salı,Çarşamba,Perşembe,Cuma,Cumartesi,Pazar};
+    private static void Main(string[] args)
+    {
+        Gun gun =Gun.Çarşamba;
+
+        if (gun == Gun.Cumartesi || gun == Gun.Pazar)
+        {
+            Console.WriteLine("Hafta Sonunu Seçtiniz");
+        }
+        else
+        {
+            Console.WriteLine("Hafta içini Seçtiniz");
+        }
+    }
+}
